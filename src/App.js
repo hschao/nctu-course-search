2f7a9dd3c93f138a2daffa4abaa91d50c0c0f6cd
Johnson Chao <chao.hs1031@gmail.com>
2018-01-25 14:29:34 +0800

    Install router and remove useless codes.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,1 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+import React from 'react'import { BrowserRouter, Route, Switch } from 'react-router-dom'const Home = () => <h1>Home</h1>const Search = () => <h1>Search</h1>const NotFound = () => <h1>404 Not Found</h1>const App = () => (    <BrowserRouter>        {/* page routes */}        <Switch>            <Route exact path='/' component={Home} />            <Route path='/search' component={Search} />            {/* 404 not found */}            <Route component={NotFound} />        </Switch>    </BrowserRouter>)export default App;
